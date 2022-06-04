083773c2df1090e914ab02b6bbe3855588e7243b
sheja <shejaemeric2@gmail.com>
2022-06-04 02:57:39 -0700

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    def printArgs(lis,num):
-        for index in range(1, num+1):
-            print("{:d}: {:s}".format(index,lis[index]))
-    argv_count = len(argv)
+
+    def printArgs(lis, num):
+        for index in range(1, num):
+            print("{:d}: {:s}".format(index, lis[index]))
+
+    argv_count = len(sys.argv)
     if argv_count == 1:
         print("{:d} arguments.".format(argv_count))
     elif argv_count == 2:
         print("{:d} argument:".format(argv_count))
-        printArgs(sys.argv,argv_count)
+        printArgs(sys.argv, argv_count)
     else:
         print("{:d} arguments:".format(argv_count))
-        printArgs(sys.argv,argv_count)
+        printArgs(sys.argv, argv_count)
