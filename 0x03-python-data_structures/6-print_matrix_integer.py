5cb5ac4fc32997959c4035b9595819265c2f60c3
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-05 17:27:14 +0300

    updates

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        count = len(row)
+        index = 1
         for item in row:
-            print("{:d}".format(item),end=" ")
+            if index == count:
+                print("{:d}".format(item))
+            else:
+                print("{:d}".format(item),end=" ")
+            index += 1
         print("")
