532c1db3761786d3d1b358707d648e1b683733c1
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-05 14:47:33 +0300

    updates

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     count = len(my_list)
-    if idx < 0 or idx > count:
+    if idx < 0 or idx > count - 1:
         return my_list
     my_list[idx] = element
     return my_list
