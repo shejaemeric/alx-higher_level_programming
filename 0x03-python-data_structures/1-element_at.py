ed1e609545ff8d5d6d2b9f0c01d42fbef9a86ad1
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-05 14:42:47 +0300

    updates

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     count = len(my_list)
-    if idx < 0 or idx > count:
+    if idx < 0 or idx > count - 1:
         return "None"
     return my_list[idx]
