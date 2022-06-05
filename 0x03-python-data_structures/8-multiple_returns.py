f9c1dbeadcb5e7db20b5359e7220a13bd046995c
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-05 16:14:35 +0300

    updates

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    count = len(sentence)
+    if count == 0:
+        first = "None"
+    else:
+        first = sentence[0]
+    new = (count,first)
