f7441f0b8c0fb1350535fd6ed3be6368c2eb0877
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-05 15:23:36 +0300

    updates

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     str = ""
-    for x in my_string
-        if x != c or x != C:
-            str += x
-    return str
+    for x in my_string:
+        if x == 'c' or x == 'C':
+            continue
+        str = str + x
+return str
+
