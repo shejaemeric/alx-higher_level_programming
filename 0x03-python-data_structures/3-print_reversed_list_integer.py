5b635b377d9b3acadc1f246e1fabe78585f2af8d
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-06 20:51:51 +0300

    updates

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    count = len(my_list) - 1
-    while (count >= 0):
-        print("{:d}".format(my_list[count]))
-        count -= 1
-
+    if my_list:
+    for x in reversed(my_list):
+        print("{:d}".format(x))
