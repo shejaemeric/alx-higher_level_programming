fada05ba6016698409535439a6721ccac93fc2b3
SHEJA Lervy Emeric <shejaemeric2@gmail.com>
2022-06-08 20:13:20 +0300

    updates

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is none:
+        return "None"
+    else:
+        a_list = list(a_dictionary)
+        count = len(a_list)
+        max = a_dictionary.get(a_list[0])
+        res = a_list[0]
+        for x in a_list[1:count]:
+            next = a_dictionary.get(x)
+            if next > max:
+                max = next
+                res = x
+        print(res)
