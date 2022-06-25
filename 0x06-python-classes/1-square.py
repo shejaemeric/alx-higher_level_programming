63dabaa6429972412571edf27858ca4795772f7b
shejaemeric <shejaemeric@gmail.com>
2022-06-25 15:26:05 +0300

    updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,11 @@
 
 
 class Square:
-    """ square class which will be used to set size, get size,
-    get area, print square..."""
-    __size
+    """Defines a square."""
 
-    def __init__(self, size=0):
-        """initialize class and needs the size
-        as arguments then returns nothing
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
         """
         self.__size = size
