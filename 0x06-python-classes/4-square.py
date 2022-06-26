9ba507de6cebb7139f550c1caca2c43ab25802ff
shejaemeric <shejaemeric2@gmail.com>
2022-06-26 20:20:17 +0300

    updates

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
         """
         if type(value) != int:
             raise TypeError("size must be an integer")
-        if value > 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
