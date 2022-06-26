852742d23e6768e5e5ae610930f49fbde7f69138
shejaemeric <shejaemeric2@gmail.com>
2022-06-26 21:15:28 +0300

    updates

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -23,7 +23,7 @@ class Square:
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
-        if value > 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
