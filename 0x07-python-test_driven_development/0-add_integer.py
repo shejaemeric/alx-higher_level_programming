086a111df648adc607887cb9503e92fc481b0dc5
SHEJAEmeric <shejaemeric2@gmail.com>
2022-07-16 16:15:07 +0300

    updates

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+        function to add 2 numbers
+        Args:
+            a (int)
+            b (int)
+        Returns:
+            int
+    """
+    if not isinstance(a,int):
+        if not isinstance(a,float):
+            raise TypeError("a must be an integer")
+        else:
+            num1 = int(a)
+
+    if not isinstance(b,int):
+        if not isinstance(b,float):
+            raise TypeError("a must be an integer")
+        else:
+            num2 = int(b)
+
+    return a+b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
