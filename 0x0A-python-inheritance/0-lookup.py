2e671b9eb31315038af5240a9b11aab62f90e514
shejaemeric <shejaemeric2@gmail.com>
2022-08-03 17:31:53 +0300

    updates

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+     module for the lookup function
+"""
+
+
+def lookup(obj):
+    """lookup : return list of all attributes in function
+
+    Args:
+        obj (class_object): object to check
+
+    Returns:
+        List: list of all atributes related to class
+    """
+    return dir(obj);
