c1c56ba638cc9c9d3418451a1ee2b080c93f9bd2
shejaemeric <shejaemeric2@gmail.com>
2022-08-03 09:56:25 +0300

    updates

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@ module to print first name and last name
 """
 
 def say_my_name(first_name, last_name=""):
+
     """
     say_my_name : prints first and last name
 
@@ -12,9 +13,9 @@ def say_my_name(first_name, last_name=""):
         last_name (str, optional): last name to print. Defaults to "".
     """
 
-    if not isinstance(first_name,str):
+    if not isinstance(first_name, tr):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name,str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name,last_name))
+    print("My name is {} {}".format(first_name, last_name))
