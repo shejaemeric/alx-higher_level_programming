5b355a18941ddd60d61b9dfbc0e3ed7c107fe1c9
shejaemeric <shejaemeric2@gmail.com>
2022-08-04 10:09:46 +0300

    updates

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    module for BaseGeometry class
+"""
+
+class BaseGeometry:
+
+    """
+        class for BaseGeometry attributes
+    """
+    def area(self):
+        """
+            area: prints that the area function has not been implemented
+        """
+        print("area() is not implemented")
+        
