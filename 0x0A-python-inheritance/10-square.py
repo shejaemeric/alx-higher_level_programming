0a6538d5ad339d22b0ce03ef4b6a035c612dc3e8
shejaemeric <shejaemeric2@gmail.com>
2022-08-04 20:07:34 +0300

    updates

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Module for square class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+
+    """Square _summary_
+
+    Args:
+        Rectangle (class): inherited class
+    """
+
+    def __init__(self, size):
+
+        """
+        __init__: initiates class square.
+
+        Args:
+            size (int): size of square.
+        """
+        self.__size = size
+        super().__init__(self.__size, self.__size)
