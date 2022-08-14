ec88c3aaa7caa3717c1fe03c3f01c947f9d7e46a
shejaemeric <shejaemeric2@gmail.com>
2022-08-14 10:17:19 +0300

    updates

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" module for a save_to_json_file function """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+
+    """
+    save_to_json_file : serialize and save to json file.
+
+    Args:
+        my_obj (object): obj to convert to json
+        filename (file): json file
+
+    """
+
+    my_str = json.loads(my_obj)
+
+    with open(filename, 'r', encoding='UTF8') as f_obj:
+        f_obj.write(my_str)
