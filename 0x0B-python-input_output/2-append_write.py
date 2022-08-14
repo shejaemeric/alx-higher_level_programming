f6b6e79a2ba187aff68141d97930003418351437
shejaemeric <shejaemeric2@gmail.com>
2022-08-14 09:51:43 +0300

    updates

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" module for write at the end of a file function """
+
+
+def append_write(filename="", text=""):
+
+    """
+    append_write: creates a file if it does not exists and wites at the end of it
+
+    Args:
+        filename (str, optional): name of the flile. Defaults to "".
+        text (str,optional): text to write in file. Defaults to "".
+    """
+    with open(filename, 'a', encoding='UTF8') as F_obj:
+        write_data = F_obj.write(text)
+        return write_data
