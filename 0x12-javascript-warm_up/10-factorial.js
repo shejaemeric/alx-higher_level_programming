859706938e47f17723513e93e0ecdec64fd9664a
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-06 00:18:35 +0200

    updates

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function fact (n) {
   if (isNaN(n) || n === 0) {
-    return 1
+    return 1;
   }
-    return n * fact (n - 1)
+  return n * fact(n - 1);
 }
-let n = parseInt(process.argv[2]);
+const n = parseInt(process.argv[2]);
 console.log(fact(n));
