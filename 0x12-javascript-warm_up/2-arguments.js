331109c2000516ae47deae310b64aeb30b617123
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-05 23:28:35 +0200

    updates

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-
-let count = process.argv.length;
-if (count === 2)
-    console.log("No argument")
-else if(count === 3)
-    console.log("Argument found")
-else
-    console.log("Arguments found")
+const count = process.argv.length;
+if (count === 2) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
