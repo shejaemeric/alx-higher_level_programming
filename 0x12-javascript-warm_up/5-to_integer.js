d944807e822d34d76d80f4af6252fb5a10c7f48e
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-06 00:14:54 +0200

    updates

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
+const arg = parseInt(process.argv[2]);
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log(`My number:${process.argv[2]}`);
+  console.log(`My number: ${arg}`);
 }
