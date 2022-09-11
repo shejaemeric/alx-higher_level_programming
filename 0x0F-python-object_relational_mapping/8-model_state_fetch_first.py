4954ff28b3e40ba43cf1e257a0af4db23cc7728e
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-11 12:47:05 +0200

    updates

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,11 +10,15 @@ engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                        .format(sys.argv[1],
                                sys.argv[2],
                                sys.argv[3]),
-                       pool_pre_ping = True)
+                       pool_pre_ping=True)
 
 session = sessionmaker(bind=engine)
 session = session()
 
 state = session.query(State).order_by(State.id).first()
-print('{}: {}'.format(state.id, state.name))
+if state:
+    print('{}: {}'.format(state.id, state.name))
+else:
+    print("Nothing")
 
+session.close()
