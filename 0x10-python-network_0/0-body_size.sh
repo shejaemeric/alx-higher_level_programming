774f2b2b32dc0c6bd11a084f458eb67a635e9b26
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-27 18:04:25 +0200

    get

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-#takes in a URL, sends a request to that URL,
-#and displays the size of response
-
+#takes in a URL, sends a request to that URL
 curl -i -s $1 | grep "Content-Length" | cut -d " " -f2
