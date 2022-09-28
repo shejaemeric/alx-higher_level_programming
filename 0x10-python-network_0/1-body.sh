8befbfcd0cb7b53998af8db25084d65c6147579c
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-28 16:00:47 +0200

    change bash

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 #takes in a URL, sends a GET request to the URL, and displays the body of the response
 #    Display only body of a 200 status code response
 #    You have to use curl
