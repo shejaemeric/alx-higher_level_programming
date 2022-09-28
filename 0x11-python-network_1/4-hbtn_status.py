eaf8c3cc453b469efe566bd03d574796a338ac3b
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-28 19:24:44 +0200

    connect using request package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     req = "https://alx-intranet.hbtn.io/status"
     res = requests.get(req)
     o = "Body response:\n\t- type: {}\n\t- content: {}"
-    print(o.format(type(res.content), res.content))
+    print(o.format(type(res.content), res.text))
