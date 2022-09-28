af203306d5b90c2b5e3a53c401243e699a0ede8e
shejaemeric2 <shejaemeric2@gmail.com>
2022-09-28 19:33:16 +0200

    connect using request package and get id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""connect and fetch using requests library and get X-Request-Id"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    req = sys.argv
+    res = requests.get(req[1])
+    print(res.headers['X-Request-Id'])
